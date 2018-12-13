d14097df1d4a242b94492b8305113991482864c0
David Hansel <david@hansels.net>
2018-12-13 14:24:17 -0500

    Added licensing info

--- ACRModem.ino
+++ ACRModem.ino
@@ -1,4 +1,21 @@
+// -----------------------------------------------------------------------------
 // Modulator/Demodulator for 300 baud Altair cassette interface
+// Copyright (C) 2018 David Hansel
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+// -----------------------------------------------------------------------------
 
 #define PIN_SERIAL_IN   2  // must be 2 or 3 (uses interrupt pin INT0 or INT1)
 #define PIN_SERIAL_OUT A4  // can be any pin
